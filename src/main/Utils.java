bc2ba9442c347242d764ac2c2bc932b39f13c24d
JVSREco19 <joao.victor.silveira.r@unifei.edu.br>
2021-10-28 14:58:09 -0300

    Part 4 - Fazendo a interface

--- src/main/Utils.java
+++ src/main/Utils.java
@@ -6,5 +6,11 @@ public class Utils {
    public static final int INIT_PLAYER_X = 405;
    public static final int INIT_PLAYER_Y = 700;
    public static final int INIT_CAR_X = 1080;
+   public static final int INIT_CAR_Y = 50;
+   public static final int CAR_HEIGHT = 40;
+   public static final int CAR_WIDTH = 55;
+   public static final int NUMBER_OF_CARS = 6;
+   public static final int DELAY = 1000;
+   public static final int PERIOD = 10;
 
 }
